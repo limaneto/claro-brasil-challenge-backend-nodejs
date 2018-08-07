7c6f3a0a089ecd56f1697c644fc15855ced9d8d5
Lima Neto <lima.neto@petcoach.co>
2018-08-06 22:45:28 -0300

    Add eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    "no-console": 0,
+    "no-underscore-dangle": 0,
+    "consistent-return": 0
+  },
+  "env": {
+    "node": true
+  },
+  "globals": {
+    "CONFIG": true
+  }
+};
