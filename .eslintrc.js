addf2a7f9b83926d76558b87d2bc9188aeebdc63
Lima Neto <lima.neto@petcoach.co>
2018-08-07 02:09:20 -0300

    New eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
   "rules": {
     "no-console": 0,
     "no-underscore-dangle": 0,
-    "consistent-return": 0
+    "consistent-return": 0,
+    "object-curly-newline": 0
   },
   "env": {
     "node": true
