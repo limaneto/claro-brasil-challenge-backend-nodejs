91b460010a0c22efad1b955810c9b2137b3746d8
Lima Neto <lima.neto@petcoach.co>
2018-08-08 02:10:58 -0300

    Remove user model

--- components/user/model.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const UserSchema = new mongoose.Schema({
-  device: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Devices', require: true }],
-  createdAt: { type: Date, default: Date.now },
-});
-
-export default mongoose.model('Users', UserSchema);
