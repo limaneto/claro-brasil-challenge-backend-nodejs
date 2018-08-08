dc5b2453020854dbc99c4f0a55c0568b22fc37e0
Lima Neto <lima.neto@petcoach.co>
2018-08-08 02:12:06 -0300

    Add user fake reference to device model

--- components/device/model.js
+++ components/device/model.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const DeviceSchema = new mongoose.Schema({
+  user: { type: Number, required: true },
   name: { type: String, minlength: 4, maxlength: 40, trim: true, required: false },
   os: { type: String, enum: ['ios', 'android'], require: false },
   createdAt: { type: Date, default: Date.now },
