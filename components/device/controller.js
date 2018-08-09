55b13917e843b8aef4e60a58584a0522fe4083b3
Lima Neto <lima.neto@petcoach.co>
2018-08-09 01:59:00 -0300

    Saving device after being validated

--- components/device/controller.js
+++ components/device/controller.js
@@ -1,7 +1,22 @@
-const add = ((req, res) => res.json({ message: req.validator.message }));
+import Device from './model';
 
-const deleteDevice = ((req, res) => res.json({ message: 'It hit put!', id: req.params.id }));
+const add = ((req, res) => {
+  if (req.validation.success) {
+    const device = new Device(req.validation.device);
+    device
+      .save()
+      .then(() => {
+        res.status(201).json({ message: 'Device registered with success!', device });
+      });
+  }
+});
 
-const edit = ((req, res) => res.json({ message: 'It hit delete!', id: req.params.id }));
+const deleteDevice = ((req, res) => {
+  res.json({ message: 'It hit put!', id: req.params.id });
+});
+
+const edit = ((req, res) => {
+  res.json({ message: 'It hit delete!', id: req.params.id });
+});
 
 export { add, deleteDevice, edit };
