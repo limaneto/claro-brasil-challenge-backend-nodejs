772b03370344fcd2f428aa5eabe671d57e8b29ab
Lima Neto <lima.neto@petcoach.co>
2018-08-09 20:07:34 -0300

    Not saving user on config anymore

--- utils/createDefaultUser.js
+++ utils/createDefaultUser.js
@@ -2,8 +2,8 @@ import User from '../components/user/model';
 import config from '../config/config';
 
 export default () => {
-  [1, 2, 3, 4, 5].forEach((item, index) => {
+  [1, 2, 3, 4, 5].forEach(() => {
     const user = new User();
-    user.save(() => { config.USERS_ID[index] = user.id; });
+    user.save(() => {});
   });
 };
