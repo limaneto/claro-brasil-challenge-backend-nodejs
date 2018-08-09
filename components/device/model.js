6f4e2ee44855e3be9aac01981f8db82509b68957
Lima Neto <lima.neto@petcoach.co>
2018-08-09 01:57:32 -0300

    Add pre save midleware to update the amount of devices saved and link to user model

--- components/device/model.js
+++ components/device/model.js
@@ -1,10 +1,19 @@
 import mongoose from 'mongoose';
+import User from '../user/model';
 
 const DeviceSchema = new mongoose.Schema({
-  user: { type: Number, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
   name: { type: String, minlength: 4, maxlength: 40, trim: true, required: false },
   os: { type: String, enum: ['ios', 'android'], require: false },
   createdAt: { type: Date, default: Date.now },
+});
+
+DeviceSchema.pre('save', function (next) {
+  User
+    .findById(this.user, (err, user) => {
+      user.devicesAmount += 1;
+      user.save(() => next());
+    });
 });
 
 export default mongoose.model('Devices', DeviceSchema);
