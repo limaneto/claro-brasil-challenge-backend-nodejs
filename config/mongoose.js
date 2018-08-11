7a8c1b6098973a7718ced88f5813e4fa53e8dd32
Lima Neto <lima.neto@petcoach.co>
2018-08-11 16:17:42 -0300

    Add port to mongoose connect

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ import config from './config';
 const db = mongoose.connection;
 
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://${config.DB_HOST}/${config.DB_NAME}`);
+§§mongoose.connect(`mongodb://${config.DB_HOST}:${config.DB_SERVER_PORT}/${config.DB_NAME}`, { server: { reconnectTries: Number.MAX_VALUE, reconnectInterval: 1000 } });
 
 db.on('error', () => { console.log('Erro on MongoDB connection'); });
 db.once('open', () => { console.log('Database connected'); });
