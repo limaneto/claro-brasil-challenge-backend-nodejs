31fd01da1b6eded62ffd70ab30d66ba69f070894
Lima Neto <lima.neto@petcoach.co>
2018-08-11 16:36:26 -0300

    Fix limit sentence

--- utils/phrases.js
+++ utils/phrases.js
@@ -1,10 +1,10 @@
 export default {
   device: {
     register: {
-      limit: 'You\'ve reached the limit of registered devices.',
+      limit: "You've reached the limit of registered devices.",
       time: 'You already registered a device within 30 last days.',
-      allow: 'You can change devices.\'',
-      nallow: 'You cannot change devices.\'',
+      allow: 'You can change devices.',
+      nallow: 'You cannot change devices.',
       nextDate: 'But you can add another from: ',
     },
     delete: {
