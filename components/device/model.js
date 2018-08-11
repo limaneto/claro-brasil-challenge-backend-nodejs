ad75842d69a69b4ce6a8e1698528352da0420e50
Lima Neto <lima.neto@petcoach.co>
2018-08-11 16:17:22 -0300

    Change device os to required

--- components/device/model.js
+++ components/device/model.js
@@ -4,7 +4,7 @@ import User from '../user/model';
 const DeviceSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
   name: { type: String, minlength: 4, maxlength: 40, trim: true, required: false },
-  os: { type: String, enum: ['ios', 'android'], require: false },
+  os: { type: String, enum: ['ios', 'android'], require: true },
   active: { type: Boolean, default: true },
   createdAt: { type: Date, default: Date.now },
 });
