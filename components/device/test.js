7968f26df5a274d2cfa7014af4c3baa5564ee052
Lima Neto <lima.neto@petcoach.co>
2018-08-11 21:37:28 -0300

    Add more tests

--- components/device/test.js
+++ components/device/test.js
@@ -1,14 +1,12 @@
-require('babel-core/register');
-require('babel-polyfill');
-
-let request = require('supertest');
+/* eslint-disable prefer-arrow-callback,func-names */
+const request = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../../app');
-import assert from 'assert';
-import config from '../../config/config';
+const assert = require('assert');
+const config = require('../../config/config');
 
-import User from '../user/model';
-import Device from './model';
+const User = require('../user/model');
+const Device = require('./model');
 
 let testUser;
 const testDevices = [];
@@ -34,19 +32,20 @@ describe('set config', function () {
 });
 
 describe('POST devices', function () {
-  it('add device without user info', function (done) {
+  it('should not add device without user info', function (done) {
     const device = new Device({ name: 'xdfrthukgepjg357j', os: 'ios' });
     request(app)
       .post('/api/devices')
       .send({ device })
       .set('Accept', 'application/json')
-      .expect(400, function (err, response) {
+      .expect(400)
+      .then((response) => {
         assert.ok(response.body.message.toLowerCase().indexOf('user') !== -1);
         done();
       });
   });
 
-  it('check that latter device was not added', function (done) {
+  it('should check that latter device was not added', function (done) {
     Device
       .findOne({ name: 'xdfrthukgepjg357j' })
       .then((device) => {
@@ -55,19 +54,20 @@ describe('POST devices', function () {
       });
   });
 
-  it('add device with wrong o. system info', function (done) {
+  it('should not add device with wrong o. system info', function (done) {
     const device = new Device({ name: 'ikuyjtreefdc', os: 'windows' });
     request(app)
       .post('/api/devices')
       .send({ userId: testUser.id, device })
       .set('Accept', 'application/json')
-      .expect(400, function (err, response) {
+      .expect(400)
+      .then((response) => {
         assert.ok(response.body.message.toLowerCase().indexOf('enum') !== -1);
         done();
       });
   });
 
-  it('check that latter device was not added', function (done) {
+  it('should check that latter device was not added', function (done) {
     Device
       .findOne({ name: 'ikuyjtreefdc' })
       .then((device) => {
@@ -76,7 +76,7 @@ describe('POST devices', function () {
       });
   });
 
-  it('add 3 devices with the same user', function (done) {
+  it('should add 3 devices with the same user', function (done) {
     for (let i = 0; i < 3; i += 1) {
       const device = new Device({ name: `Xiomi Mi ${i}`, os: 'android' });
       request(app)
@@ -98,15 +98,14 @@ describe('POST devices', function () {
     }
   });
 
-  it('there are 3 and only 3 devices saved?', function (done) {
+  it('should only have 3 devices on the database', function (done) {
     Device
       .find()
-      .then((devices) => { assert.equal(devices.length, 3); })
-      .catch((err) => { done(err); })
-      .finally(done);
+      .then((devices) => { assert.equal(devices.length, 3); done(); })
+      .catch((err) => { done(err); });
   });
 
-  it('devices matches the ones added?', function (done) {
+  it('should check that database devices match the ones added', function (done) {
     Device
       .find()
       .then((devices) => {
@@ -114,30 +113,77 @@ describe('POST devices', function () {
           const device = devices.filter(currentDevice => currentDevice.name === `Xiomi Mi ${index}` && (currentDevice.user + '') === testUser.id);
           assert.equal(device.length, 1);
         });
+        done();
       })
-      .catch((err) => { done(err); })
-      .finally(done);
+      .catch((err) => { done(err); });
   });
 
-  it('cannot add a fourth device within 30 days', function (done) {
+  it('should not be able to add a fourth', function (done) {
     const device = new Device({ name: 'xdfrthukgepjg357j', os: 'ios' });
     request(app)
       .post('/api/devices')
       .send({ userId: testUser.id, device })
       .set('Accept', 'application/json')
-      .expect(400, function (err, response) {
+      .expect(400)
+      .then((response) => {
         assert.ok(response.body.message.toLowerCase().indexOf('limit') !== -1);
         done();
       });
   });
 
-  it('delete device after adding 3', function (done) {
+  it('should delete a device', function (done) {
     request(app)
-      .post(`/api/devices/${testDevices[0].id}`)
+      .delete(`/api/devices/${testDevices[0]._id}`)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200, function (response) {
-        console.log(response.body);
+      .expect(200)
+      .then((response) => {
+        assert.ok(response.body.message.toLowerCase().indexOf('deleted') !== -1);
+        done();
+      });
+  });
+
+  it('should check if latter device was deleted', function (done) {
+    Device
+      .findById(testDevices[0]._id)
+      .then((device) => {
+        assert.ok(device.active === false);
+        done();
+      });
+  });
+
+  it('should add a fourth', function (done) {
+    const device = new Device({ name: 'olikyujthrgt', os: 'android' });
+    request(app)
+      .post('/api/devices')
+      .send({ userId: testUser.id, device })
+      .set('Accept', 'application/json')
+      .expect(201)
+      .then((response) => {
+        testDevices.push(response.body.device);
+        assert.equal(response.body.message, 'Device registered with success!');
+        assert.equal(typeof response.body.device, 'object');
+        assert.equal(response.body.device.user, testUser.id);
+        assert.equal(response.body.device.active, true);
+        assert.equal(response.body.device.name, 'olikyujthrgt');
+        assert.equal(response.body.device.os, 'android');
+        done();
+      })
+      .catch(err => done(err));
+  });
+
+  it('should not be able to add a fifth', function (done) {
+    const device = new Device({ name: 'xdfrthukgepjg357j', os: 'ios' });
+    request(app)
+      .post('/api/devices')
+      .send({ userId: testUser.id, device })
+      .set('Accept', 'application/json')
+      .expect(400)
+      .then((response) => {
+        const limit = response.body.message.toLowerCase().indexOf('limit') !== -1;
+        const days = response.body.message.toLowerCase().indexOf('30') !== -1;
+        const noChange = response.body.message.toLowerCase().indexOf('cannot change') !== -1;
+        assert.ok(limit && days && noChange);
         done();
       });
   });
