c3ed2c58197e18e7c5052980d57916eb1182750e
Lima Neto <lima.neto@petcoach.co>
2018-08-13 00:27:08 -0300

    Print default users for test.

--- utils/createDefaultUser.js
+++ utils/createDefaultUser.js
@@ -3,6 +3,8 @@ const User = require('../components/user/model');
 module.exports = () => {
   [1, 2, 3, 4, 5].forEach(() => {
     const user = new User();
-    user.save(() => {});
+    user.save(() => {
+      console.log(user);
+    });
   });
 };
